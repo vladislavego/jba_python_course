cf9cb53511d9e3b21c68f5432db114cf491337ba
vladislavego <ego.v.a@ya.ru>
2020-10-04 14:40:48 +0300

    Tic-tac-toe with AI project

--- tic_tac_toe_with_AI/tic_tac_toe_with_AI.py
+++ tic_tac_toe_with_AI/tic_tac_toe_with_AI.py
@@ -80,7 +80,7 @@ class GameField:
 class Players:
     players_number = 0
 
-    def __new__(cls, player_type="user", field_state=None):
+    def __new__(cls, player_type="user"):
         Players.players_number += 1
         if player_type == "user":
             user = object.__new__(User)
@@ -101,8 +101,7 @@ class User(Players):
     users_number = 0
 
     def __init__(self, name=None):
-        self.class_number = User.users_number
-        self.number = Players.players_number
+        pass
 
     @staticmethod
     def __is_initial_correct(initial_cells: str) -> bool:
@@ -145,17 +144,22 @@ class Computer(Players):
 
     def __init__(self, level: str):
         level = level
-        self.class_number = Computer.computers_number
-        self.number = Players.players_number
 
-    def is_pre_win(self, player_number: int) -> list:
+    def is_pre_win_cells(self, player_number: int) -> list:
         win_cells = []
-        for pre_win in self.find_pre_win_cells(player_number):
+        pre_win_cells = self.__find_interesting_cells(player_number)[0]
+        for pre_win in pre_win_cells:
             win_cells.extend(pre_win)
         return win_cells
 
-    @staticmethod
-    def find_pre_win_cells(player_number: int) -> tuple:
+    def is_priority_cells(self, player_number: int) -> list:
+        cells = []
+        priority_cells = self.__find_interesting_cells(player_number)[1]
+        for priority in priority_cells:
+            cells.extend(priority)
+        return cells
+
+    def __find_interesting_cells(self, player_number: int) -> tuple:
         marker = 'X'
         if player_number % 2 == 0:
             marker = 'O'
@@ -163,7 +167,15 @@ class Computer(Players):
         columns = game.game_field.get_column_list()
         side_diagonal = [columns[i][i] for i in range(3)]
         main_diagonal = [rows[i][i] for i in range(3)]
-        # line with index i in rows have 2 - i index in game_field
+        more_priority_cells = ()
+        pre_win_cells = self.__find_pre_win_cells(marker, rows, columns, main_diagonal, side_diagonal)
+        if self.level == "hard":
+            more_priority_cells = self.__find_priority_cells(marker, rows, columns, main_diagonal, side_diagonal)
+        return pre_win_cells, more_priority_cells
+
+    @staticmethod
+    def __find_pre_win_cells(marker, rows, columns, main_diagonal, side_diagonal):
+        # line with index i in rows have i + 2 index in game_field
         # to get game_field line index: 2 - i
         is_pre_win_row = [(n, 2 - m) for m in range(3) for n in range(3)
                           if rows[m].count(marker) == 2 and rows[m][n] == ' ']
@@ -176,20 +188,77 @@ class Computer(Players):
         return is_pre_win_row, is_pre_win_column, is_pre_win_main_diagonal, is_pre_win_side_diagonal
 
     @staticmethod
+    def __find_priority_cells(marker, rows, columns, main_diagonal, side_diagonal):
+        is_row_with_single_symbol = [(n, 2 - m) for m in range(3) for n in range(3)
+                                     if rows[m].count(marker) == 1 and rows[m].count(' ') == 2 and
+                                     rows[m][n] == ' ']
+        is_column_with_single_symbol = [(n, m) for m in range(3) for n in range(3)
+                                        if columns[n].count(marker) == 1 and columns[n].count(' ') == 2 and
+                                        columns[n][m] == ' ']
+        is_main_diagonal_with_single_symbol = [(i, 2 - i) for i in range(3)
+                                               if main_diagonal.count(marker) == 1 and main_diagonal.count(' ') == 2 and
+                                               main_diagonal[i] == ' ']
+        is_side_diagonal_with_single_symbol = [(i, i) for i in range(3)
+                                               if side_diagonal.count(marker) == 1 and side_diagonal.count(' ') == 2 and
+                                               side_diagonal[i] == ' ']
+        return is_row_with_single_symbol, is_column_with_single_symbol, \
+            is_main_diagonal_with_single_symbol, is_side_diagonal_with_single_symbol
+
+    @staticmethod
     def __generate_cell() -> tuple:
         return random.randint(0, 2), random.randint(0, 2)
 
     def generate_cell_to_fill(self) -> tuple:
         if self.level == "easy":
             return self.__generate_cell()
-        computer_win_cells = self.is_pre_win(self.number)
-        user_win_cells = self.is_pre_win(self.number + 1)
+        win_cell = self.__generate_cell_medium()
+        if win_cell:
+            return win_cell
+        if self.level == "hard":
+            choice = random.randint(1, 10)
+            if choice % 2 == 0:
+                priority_cell = self.__generate_cell_hard_free()
+            else:
+                priority_cell = self.__generate_cell_hard_block()
+            if priority_cell:
+                return priority_cell
+        return self.__generate_cell()
+
+    def __generate_cell_medium(self) -> tuple:
+        computer_win_cells = self.is_pre_win_cells(self.number)
+        user_win_cells = self.is_pre_win_cells(self.number + 1)
         if computer_win_cells:
             cell = random.choice(computer_win_cells)
             computer_win_cells.remove(cell)
         elif user_win_cells:
             cell = random.choice(user_win_cells)
             user_win_cells.remove(cell)
+        else:
+            cell = ()
+        return cell
+
+    def __generate_cell_hard_block(self) -> tuple:
+        computer_priority_cells = self.is_priority_cells(self.number)
+        user_priority_cells = self.is_priority_cells(self.number + 1)
+        if user_priority_cells:
+            cell = random.choice(user_priority_cells)
+            user_priority_cells.remove(cell)
+        elif computer_priority_cells:
+            cell = random.choice(computer_priority_cells)
+            computer_priority_cells.remove(cell)
+        else:
+            cell = self.__generate_cell()
+        return cell
+
+    def __generate_cell_hard_free(self) -> tuple:
+        computer_priority_cells = self.is_priority_cells(self.number)
+        user_priority_cells = self.is_priority_cells(self.number + 1)
+        if computer_priority_cells:
+            cell = random.choice(computer_priority_cells)
+            computer_priority_cells.remove(cell)
+        elif user_priority_cells:
+            cell = random.choice(user_priority_cells)
+            user_priority_cells.remove(cell)
         else:
             cell = self.__generate_cell()
         return cell
@@ -206,7 +275,7 @@ class Game:
 
     @staticmethod
     def __is_correct_user_input(user_input: list) -> int:
-        correct_parameters = ["easy", "user", "medium"]
+        correct_parameters = ["easy", "user", "medium", "hard"]
         length = len(user_input)
         play_game = length == 3 and user_input[0] == "start" and user_input[1] in correct_parameters and \
             user_input[2] in correct_parameters
@@ -218,8 +287,11 @@ class Game:
     @staticmethod
     def __get_user_choice():
         user_input = input("""Input "start player1 player2" to play.
-player1 and player2 can only have "user", "easy" or "medium" value. 
-For example: print "start easy easy" to watch AI battle or "start medium user" to compete with it.           
+player1 and player2 can only have "user", "easy", "medium" or "hard" value. 
+For example: print "start easy easy" to watch AI battle.
+To compete with it print "start user medium" if you want to move first or "start medium user" in other case. 
+For hardcore choose "hard" opponent.          
+
 Input "exit" to exit game.
 
 Input command: """).split()
